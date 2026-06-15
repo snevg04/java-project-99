8cc1a58e17e7287ddf822646f8178bc89b669e45
snevg04 <98708452+snevg04@users.noreply.github.com>
2026-06-15 17:17:58 +1000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
 	java
 	id("org.springframework.boot") version "4.0.6"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.sonarqube") version "7.3.1.8318"
+	jacoco
+    checkstyle
 }
 
 group = "hexlet.code"
@@ -25,6 +28,31 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+sonar {
+  properties {
+    property("sonar.projectKey", "snevg04_java-project-99")
+    property("sonar.organization", "sn-evg-04")
+	property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${layout.buildDirectory.get()}/reports/jacoco/test/jacocoTestReport.xml"
+	)
+  }
+}
+
+jacoco {
+    toolVersion = "0.8.13"
+}
+
+tasks.named<JacocoReport>("jacocoTestReport") {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
 }
