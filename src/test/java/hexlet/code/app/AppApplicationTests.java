f46b6c6e29316725e918b64f0b1b752028b06c03
snevg04 <evg.sneg.117@yandex.ru>
2026-06-17 23:38:51 +1000

    tests work-in-progress

--- src/test/java/hexlet/code/app/AppApplicationTests.java
+++ src/test/java/hexlet/code/app/AppApplicationTests.java
@@ -1,13 +1,78 @@
 package hexlet.code.app;
 
+import hexlet.code.app.model.User;
+import net.datafaker.Faker;
+import org.instancio.Instancio;
+import org.instancio.Select;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AppApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final Faker faker = new Faker();
+
     @Test
-    void contextLoads() {
+    public void testWelcome() throws Exception {
+        mockMvc.perform(get("/welcome"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+        var result = mockMvc.perform(get("/api/users"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        assertThatJson(body).isArray();
+    }
+    @Test
+    public void testCreate() throws Exception {
+        var user = Instancio.of(User.class)
+                .ignore(Select.field(User::getId))
+                .supply(Select.field(User::getFirstName), () -> faker.name().firstName())
+                .supply(Select.field(User::getLastName), () -> faker.name().lastName())
+                .supply(Select.field(User::getEmail), () -> faker.internet().emailAddress())
+                .supply(Select.field(User::getPassword), () -> faker.internet().password())
+                .create();
+
+
+
+        var result = mockMvc.perform(post("/api/users/"))
+                .andExpect(status().isCreated());
+
+
+    }
+
+    @Test
+    public void testShow() throws Exception {
+        mockMvc.perform(get("/api/users/{id}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        mockMvc.perform(get("/api/users/{id}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        mockMvc.perform(get("/api/users/{id}"))
+                .andExpect(status().isNoContent());
     }
 
 }
