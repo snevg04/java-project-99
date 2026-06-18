483a671bcbd296f9785b72eb7c0d7af6343fc0d1
snevg04 <evg.sneg.117@yandex.ru>
2026-06-18 22:26:36 +1000

    add UserService

---
+++ src/main/java/hexlet/code/app/service/UserService.java
@@ -0,0 +1,83 @@
+package hexlet.code.app.service;
+
+import hexlet.code.app.dto.UserCreateDTO;
+import hexlet.code.app.dto.UserDTO;
+import hexlet.code.app.dto.UserUpdateDTO;
+import hexlet.code.app.exception.ResourceNotFoundException;
+import hexlet.code.app.model.User;
+import hexlet.code.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserDTO> getAll() {
+        var users = userRepository.findAll();
+        return users.stream()
+                .map(this::toDTO)
+                .toList();
+    }
+
+    public UserDTO createUser(UserCreateDTO userCreateDTO) {
+        var user = toEntity(userCreateDTO);
+        var savedUser = userRepository.save(user);
+        return toDTO(savedUser);
+    }
+
+    public UserDTO showUser(Long id) {
+        var user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found!"));
+        return toDTO(user);
+    }
+
+    public UserDTO updateUser(UserUpdateDTO userUpdateDTO, Long id) {
+        var user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found!"));
+        updateEntity(userUpdateDTO, user);
+        var savedUser = userRepository.save(user);
+        return toDTO(savedUser);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public UserDTO toDTO(User user) {
+        var userDto = new UserDTO();
+        userDto.setId(user.getId());
+        userDto.setFirstName(user.getFirstName());
+        userDto.setLastName(user.getLastName());
+        userDto.setEmail(user.getEmail());
+        userDto.setCreatedAt(user.getCreatedAt());
+        userDto.setUpdatedAt(user.getUpdatedAt());
+        return userDto;
+    }
+
+    public User toEntity(UserCreateDTO userCreateDTO) {
+        var user = new User();
+        user.setFirstName(userCreateDTO.getFirstName());
+        user.setLastName(userCreateDTO.getLastName());
+        user.setEmail(userCreateDTO.getEmail());
+        return user;
+    }
+
+    public void updateEntity(UserUpdateDTO userUpdateDTO, User user) {
+
+        if (userUpdateDTO.getFirstName() != null) {
+            user.setFirstName(userUpdateDTO.getFirstName());
+        }
+
+        if (userUpdateDTO.getLastName() != null) {
+            user.setLastName(userUpdateDTO.getLastName());
+        }
+        if (userUpdateDTO.getEmail() != null) {
+            user.setEmail(userUpdateDTO.getEmail());
+        }
+    }
+}
