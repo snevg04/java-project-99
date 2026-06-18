9df5c272cf30dd7cec30968cf8e631d2e3fee879
snevg04 <evg.sneg.117@yandex.ru>
2026-06-18 22:38:51 +1000

    add validation annotations to DTOs

--- src/main/java/hexlet/code/app/dto/UserCreateDTO.java
+++ src/main/java/hexlet/code/app/dto/UserCreateDTO.java
@@ -1,5 +1,9 @@
 package hexlet.code.app.dto;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,5 +12,13 @@ import lombok.Setter;
 public class UserCreateDTO {
     private String firstName;
     private String lastName;
+
+    @NotBlank
+    @Column(unique = true)
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 3)
+    private String password;
 }
--- src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
+++ src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
@@ -1,5 +1,7 @@
 package hexlet.code.app.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,5 +10,8 @@ import lombok.Setter;
 public class UserUpdateDTO {
     private String firstName;
     private String lastName;
+    @Email
     private String email;
+    @Size(min = 3)
+    private String password;
 }
