fa84def33ad84d75ab454badcb04058ce61fdd49
snevg04 <evg.sneg.117@yandex.ru>
2026-06-20 14:06:00 +1000

    add findByEmail method

--- src/main/java/hexlet/code/app/repository/UserRepository.java
+++ src/main/java/hexlet/code/app/repository/UserRepository.java
@@ -3,5 +3,8 @@ package hexlet.code.app.repository;
 import hexlet.code.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByEmail(String email);
 }
