27b6f2f028aabd48ea2dcd46c773391534e66167
snevg04 <evg.sneg.117@yandex.ru>
2026-06-20 10:39:46 +1000

    remove blank space

--- src/main/java/hexlet/code/app/repository/UserRepository.java
+++ src/main/java/hexlet/code/app/repository/UserRepository.java
@@ -3,5 +3,5 @@ package hexlet.code.app.repository;
 import hexlet.code.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository  extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
