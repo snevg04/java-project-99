72ac0c41ec9943bfc48c4fe540253390daa03530
Evgenii <evg.sneg.117@yandex.ru>
2026-06-21 18:02:53 +1000

    update User model

--- src/main/java/hexlet/code/app/model/User.java
+++ src/main/java/hexlet/code/app/model/User.java
@@ -47,7 +47,12 @@ public class User implements UserDetails {
 
     @Override
     public String getUsername() {
-        return "";
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
     }
 
     @Override
