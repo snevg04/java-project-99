2a2909b0d137d9d318a1ea1f845e85e36044fb0f
Evgenii <evg.sneg.117@yandex.ru>
2026-06-21 17:58:37 +1000

    update CustomUserDetailsService

--- src/main/java/hexlet/code/app/service/CustomUserDetailsService.java
+++ src/main/java/hexlet/code/app/service/CustomUserDetailsService.java
@@ -3,12 +3,12 @@ package hexlet.code.app.service;
 import hexlet.code.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsManager {
+public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
@@ -18,30 +18,5 @@ public class CustomUserDetailsService implements UserDetailsManager {
         var user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
         return user;
-    }
-
-    @Override
-    public void createUser(UserDetails user) {
-
-    }
-
-    @Override
-    public void updateUser(UserDetails user) {
-
-    }
-
-    @Override
-    public void deleteUser(String username) {
-
-    }
-
-    @Override
-    public void changePassword(String oldPassword, String newPassword) {
-
-    }
-
-    @Override
-    public boolean userExists(String username) {
-        return false;
     }
 }
