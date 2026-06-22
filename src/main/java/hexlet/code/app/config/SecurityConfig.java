947edab4c28a8af7b6e160f68614892d95c6a929
snevg04 <evg.sneg.117@yandex.ru>
2026-06-22 16:44:29 +1000

    update SecurityConfig

--- src/main/java/hexlet/code/app/config/SecurityConfig.java
+++ src/main/java/hexlet/code/app/config/SecurityConfig.java
@@ -46,35 +46,23 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
-
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-
                 .authorizeHttpRequests(auth -> auth
-
-                        // фронтенд
                         .requestMatchers(
                                 "/",
                                 "/index.html",
                                 "/favicon.ico",
                                 "/assets/**"
                         ).permitAll()
-
-                        // логин
                         .requestMatchers("/api/login").permitAll()
-
-                        // всё API требует JWT
                         .requestMatchers("/api/**").authenticated()
-
-                        // всё остальное открываем
                         .anyRequest().permitAll()
                 )
-
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt -> jwt.decoder(jwtDecoder))
                 )
-
                 .build();
     }
 }
