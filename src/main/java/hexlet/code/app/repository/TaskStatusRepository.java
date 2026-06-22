d9240704912212cc021d143a334b6b6c18900912
snevg04 <evg.sneg.117@yandex.ru>
2026-06-22 14:47:39 +1000

    add existsBy methods

--- src/main/java/hexlet/code/app/repository/TaskStatusRepository.java
+++ src/main/java/hexlet/code/app/repository/TaskStatusRepository.java
@@ -2,9 +2,12 @@ package hexlet.code.app.repository;
 
 import hexlet.code.app.model.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.scheduling.config.Task;
 
 import java.util.Optional;
 
 public interface TaskStatusRepository extends JpaRepository<TaskStatus, Long> {
     public Optional<TaskStatus> findBySlug(String slug);
+    boolean existsBySlug(String slug);
+    boolean existsByName(String name);
 }
