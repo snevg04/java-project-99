f078aa461d31ceb3916d61cdb29f0cafe8e46acb
snevg04 <evg.sneg.117@yandex.ru>
2026-06-22 14:25:21 +1000

    refactor UserService with UserMapper

--- src/main/java/hexlet/code/app/service/UserService.java
+++ src/main/java/hexlet/code/app/service/UserService.java
@@ -4,6 +4,7 @@ import hexlet.code.app.dto.UserCreateDTO;
 import hexlet.code.app.dto.UserDTO;
 import hexlet.code.app.dto.UserUpdateDTO;
 import hexlet.code.app.exception.ResourceNotFoundException;
+import hexlet.code.app.mapper.UserMapper;
 import hexlet.code.app.model.User;
 import hexlet.code.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,71 +22,43 @@ public class UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private UserMapper userMapper;
+
     public List<UserDTO> getAll() {
         var users = userRepository.findAll();
         return users.stream()
-                .map(this::toDTO)
+                .map(userMapper::toDTO)
                 .toList();
     }
 
     public UserDTO createUser(UserCreateDTO userCreateDTO) {
-        var user = toEntity(userCreateDTO);
+        var user = userMapper.toEntity(userCreateDTO);
         user.setPasswordDigest(
                 passwordEncoder.encode(userCreateDTO.getPassword())
         );
 
         var savedUser = userRepository.save(user);
-        return toDTO(savedUser);
+        return userMapper.toDTO(savedUser);
     }
 
     public UserDTO showUser(Long id) {
         var user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found!"));
-        return toDTO(user);
+        return userMapper.toDTO(user);
     }
 
     public UserDTO updateUser(UserUpdateDTO userUpdateDTO, Long id) {
         var user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found!"));
-        updateEntity(userUpdateDTO, user);
+        userMapper.updateEntity(userUpdateDTO, user);
         var savedUser = userRepository.save(user);
-        return toDTO(savedUser);
+        return userMapper.toDTO(savedUser);
     }
 
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
 
-    public UserDTO toDTO(User user) {
-        var userDto = new UserDTO();
-        userDto.setId(user.getId());
-        userDto.setFirstName(user.getFirstName());
-        userDto.setLastName(user.getLastName());
-        userDto.setEmail(user.getEmail());
-        userDto.setCreatedAt(user.getCreatedAt());
-        userDto.setUpdatedAt(user.getUpdatedAt());
-        return userDto;
-    }
 
-    public User toEntity(UserCreateDTO userCreateDTO) {
-        var user = new User();
-        user.setFirstName(userCreateDTO.getFirstName());
-        user.setLastName(userCreateDTO.getLastName());
-        user.setEmail(userCreateDTO.getEmail());
-        return user;
-    }
-
-    public void updateEntity(UserUpdateDTO userUpdateDTO, User user) {
-
-        if (userUpdateDTO.getFirstName() != null) {
-            user.setFirstName(userUpdateDTO.getFirstName());
-        }
-
-        if (userUpdateDTO.getLastName() != null) {
-            user.setLastName(userUpdateDTO.getLastName());
-        }
-        if (userUpdateDTO.getEmail() != null) {
-            user.setEmail(userUpdateDTO.getEmail());
-        }
-    }
 }
