3bb8678159a9eb2f471d742f4a1a9406d134496a
snevg04 <evg.sneg.117@yandex.ru>
2026-06-23 21:58:41 +1000

    update TaskMapper

--- src/main/java/hexlet/code/app/mapper/TaskMapper.java
+++ src/main/java/hexlet/code/app/mapper/TaskMapper.java
@@ -3,11 +3,22 @@ package hexlet.code.app.mapper;
 import hexlet.code.app.dto.TaskCreateDTO;
 import hexlet.code.app.dto.TaskDTO;
 import hexlet.code.app.dto.TaskUpdateDTO;
+import hexlet.code.app.exception.ResourceNotFoundException;
 import hexlet.code.app.model.Task;
+import hexlet.code.app.repository.TaskStatusRepository;
+import hexlet.code.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TaskMapper {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
     public TaskDTO toDTO(Task task) {
         var taskDto = new TaskDTO();
         taskDto.setId(task.getId());
@@ -15,8 +26,8 @@ public class TaskMapper {
         taskDto.setCreatedAt(task.getCreatedAt());
         taskDto.setName(task.getName());
         taskDto.setDescription(task.getDescription());
-        taskDto.setAssignee(task.getAssignee());
-        taskDto.setTaskStatus(task.getTaskStatus());
+        taskDto.setAssigneeId(task.getAssignee().getId());
+        taskDto.setTaskStatusId(task.getTaskStatus().getId());
         return taskDto;
     }
 
@@ -25,8 +36,10 @@ public class TaskMapper {
         task.setIndex(taskCreateDTO.getIndex());
         task.setName(taskCreateDTO.getName());
         task.setDescription(taskCreateDTO.getDescription());
-        task.setAssignee(taskCreateDTO.getAssignee());
-        task.setTaskStatus(taskCreateDTO.getTaskStatus());
+        task.setAssignee(userRepository.findById(taskCreateDTO.getAssigneeId())
+                .orElseThrow(() -> new ResourceNotFoundException("Assignee not found!")));
+        task.setTaskStatus(taskStatusRepository.findById(taskCreateDTO.getTaskStatusId())
+                .orElseThrow(() -> new ResourceNotFoundException("Task status not found!")));
         return task;
     }
 
@@ -45,12 +58,14 @@ public class TaskMapper {
             task.setDescription(taskUpdateDTO.getDescription());
         }
 
-        if (taskUpdateDTO.getAssignee() != null) {
-            task.setAssignee(taskUpdateDTO.getAssignee());
+        if (taskUpdateDTO.getAssigneeId() != null) {
+            task.setAssignee(userRepository.findById(taskUpdateDTO.getAssigneeId())
+                    .orElseThrow(() -> new ResourceNotFoundException("Assignee not found!")));
         }
 
-        if (taskUpdateDTO.getTaskStatus() != null) {
-            task.setTaskStatus(taskUpdateDTO.getTaskStatus());
+        if (taskUpdateDTO.getTaskStatusId() != null) {
+            task.setTaskStatus(taskStatusRepository.findById(taskUpdateDTO.getTaskStatusId())
+                    .orElseThrow(() -> new ResourceNotFoundException("Task status not found!")));
         }
     }
 }
