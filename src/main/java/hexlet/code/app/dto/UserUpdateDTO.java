f57b6c72f9666c10035fad33fd6cd64f97bfe6a8
snevg04 <evg.sneg.117@yandex.ru>
2026-06-23 13:09:18 +1000

    add NotBlank annotation

--- src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
+++ src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
@@ -1,6 +1,7 @@
 package hexlet.code.app.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +12,7 @@ public class UserUpdateDTO {
     private String firstName;
     private String lastName;
     @Email
+    @NotBlank
     private String email;
     @Size(min = 3)
     private String password;
