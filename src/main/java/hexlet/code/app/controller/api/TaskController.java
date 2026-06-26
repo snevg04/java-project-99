77ed1abec4bcc5127e47ae87480e69df852f31cd
snevg04 <evg.sneg.117@yandex.ru>
2026-06-27 00:28:53 +1000

    add TaskController

--- src/main/java/hexlet/code/app/controller/api/TaskController.java
+++ src/main/java/hexlet/code/app/controller/api/TaskController.java
@@ -2,10 +2,12 @@ package hexlet.code.app.controller.api;
 
 import hexlet.code.app.dto.TaskCreateDTO;
 import hexlet.code.app.dto.TaskDTO;
+import hexlet.code.app.dto.TaskParamsDTO;
 import hexlet.code.app.dto.TaskUpdateDTO;
 import hexlet.code.app.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,8 +30,8 @@ public class TaskController {
     private TaskService taskService;
 
     @GetMapping("")
-    public List<TaskDTO> index() {
-        return taskService.getAllTasks();
+    public Page<TaskDTO> index(TaskParamsDTO params, @RequestParam(defaultValue = "5") int page) {
+        return taskService.getAllTasks(params, page);
     }
 
     @PostMapping("")
