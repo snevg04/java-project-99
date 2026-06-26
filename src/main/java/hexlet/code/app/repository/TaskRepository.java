e65cb7153ff343de3048f29387a8714c4d3ea1e6
snevg04 <evg.sneg.117@yandex.ru>
2026-06-27 00:28:47 +1000

    add TaskRepository

--- src/main/java/hexlet/code/app/repository/TaskRepository.java
+++ src/main/java/hexlet/code/app/repository/TaskRepository.java
@@ -2,6 +2,7 @@ package hexlet.code.app.repository;
 
 import hexlet.code.app.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
 }
