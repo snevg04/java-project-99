e659435b0d06f7f9fb25c77142191b0a5afd7f57
Evgenii <evg.sneg.117@yandex.ru>
2026-07-27 18:35:45 +0500

    fix dependency injection

--- src/main/java/hexlet/code/service/CustomUserDetailsService.java
+++ src/main/java/hexlet/code/service/CustomUserDetailsService.java
@@ -2,7 +2,7 @@ package hexlet.code.service;
 
 import hexlet.code.model.User;
 import hexlet.code.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,13 +10,12 @@ import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsManager {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void changePassword(String oldPassword, String newPassword) {
