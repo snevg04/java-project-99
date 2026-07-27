a54901fdc803a6a9f93941f84b5ce127122c9e0d
Evgenii <evg.sneg.117@yandex.ru>
2026-07-27 18:38:13 +0500

    remove unused import

--- src/main/java/hexlet/code/controller/api/UserController.java
+++ src/main/java/hexlet/code/controller/api/UserController.java
@@ -6,7 +6,6 @@ import hexlet.code.dto.UserUpdateDTO;
 import hexlet.code.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
