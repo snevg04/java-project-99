e8b260a3c06604d5e7c530f5b789cdb4557adbd0
Evgenii <evg.sneg.117@yandex.ru>
2026-07-27 20:32:08 +0500

    fix N+1 issue

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -1,8 +1,14 @@
 package hexlet.code.repository;
 
 import hexlet.code.model.Task;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
+    @EntityGraph(attributePaths = {"taskStatus", "assignee", "labels"})
+    List<Task> findAll(Specification<Task> spec);
 }
