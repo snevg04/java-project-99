970496b066ec2ab79d0771c47c87c9d72a78e3d0
Evgenii <evg.sneg.117@yandex.ru>
2026-07-28 09:50:18 +0500

    try to fix hexlet-test issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencyManagement {
 }
 
 dependencies {
+    testImplementation(project(":code:"))
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
