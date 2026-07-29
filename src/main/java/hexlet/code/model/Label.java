8bf98269fe261597e6e5384a4bfc0beca411b321
Evgenii <evg.sneg.117@yandex.ru>
2026-07-29 16:32:28 +0500

    replace ArrayList with HashSet

--- src/main/java/hexlet/code/model/Label.java
+++ src/main/java/hexlet/code/model/Label.java
@@ -16,7 +16,8 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -42,5 +43,5 @@ public class Label implements BaseEntity {
     private LocalDate createdAt;
 
     @ManyToMany(mappedBy = "labels")
-    private List<Task> tasks;
+    private Set<Task> tasks = new HashSet<>();
 }
